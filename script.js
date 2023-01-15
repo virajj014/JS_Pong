df60606e04009e6d118b43c4731f2e363a0511d5
harshal <harshal.jain.csai.20@ggits.net>
2023-01-15 18:15:11 +0530

    day1

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let userpaddle = document.getElementById("userpaddle");
-let aiPaddle = document.getElementById("aiPaddle");
+let aipaddle = document.getElementById("aipaddle");
 let ball = document.getElementById("ball");
 let gamebox = document.getElementById("gamebox");
 let zpressed = false;
 let xpressed = false;
+
+// 
+let userscore = document.getElementById("userscore");
+let aiscore = document.getElementById("aiscore");
+// 
 
 document.addEventListener("keydown", keyDownHandler);
 document.addEventListener("keyup", keyUpHandler);
@@ -55,12 +60,45 @@ function reset() {
 }
 
 
+function checkcollision(activepaddle) {
+    let balltop = ball.offsetTop;
+    let ballbottom = ball.offsetTop + ball.offsetHeight;
+    let ballleft = ball.offsetLeft;
+    let ballright = ball.offsetLeft + ball.offsetWidth;
+
+    let paddletop = activepaddle.offsetTop;
+    let paddlebottom = activepaddle.offsetTop + activepaddle.offsetHeight;
+    let paddleleft = activepaddle.offsetLeft;
+    let paddleright = activepaddle.offsetLeft + activepaddle.offsetWidth;
+
+    // console.log(balltop, ballbottom, ballleft, ballright);
+    // console.log(paddletop, paddlebottom, paddleleft, paddleright);
+
+    if (
+        ballbottom > paddletop && balltop < paddlebottom &&
+        ballright > paddleleft &&
+        ballleft < paddleright
+    ) {
+        // console.log("collision detected");
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
+
+
 function gameloop() {
     if (ball.offsetLeft < 0) {
-       reset();
+        aiscore.innerHTML = parseInt(aiscore.innerHTML) + 1;
+           reset();
+        // Vx = -Vx;
     }
     if (ball.offsetLeft > gamebox.offsetWidth - ball.offsetWidth) {
+        userscore.innerHTML = parseInt(userscore.innerHTML) + 1;
         reset();
+        // Vx = -Vx;
     }
     if (ball.offsetTop < 0) {
         Vy = -Vy;
@@ -68,6 +106,48 @@ function gameloop() {
     if (ball.offsetTop > gamebox.offsetHeight - ball.offsetHeight) {
         Vy = -Vy;
     }
+    // ///////////////
+    let paddle = ball.offsetLeft < gamebox.offsetWidth / 2 ? userpaddle : aipaddle;
+
+    let ballcenterY = ball.offsetTop + ball.offsetHeight / 2;
+    let paddlecenterY = paddle.offsetTop + paddle.offsetHeight / 2;
+
+    let angle = 0;
+
+    if (checkcollision(paddle)) {
+        if (paddle == userpaddle) {
+            if (ballcenterY < paddlecenterY) {
+                angle = -Math.PI / 4;
+            }
+            else if (ballcenterY > paddlecenterY) {
+                angle = Math.PI / 4;
+            }
+            else {
+                angle = 0;
+            }
+        }
+        else if (paddle == aipaddle) {
+            if (ballcenterY < paddlecenterY) {
+                angle = -3 * Math.PI / 4;
+            }
+            else if (ballcenterY > paddlecenterY) {
+                angle = 3 * Math.PI / 4;
+            }
+            else {
+                angle = 0;
+            }
+        }
+        V = V + 0.2;
+        Vx = V * Math.cos(angle);
+        Vy = V * Math.sin(angle);
+    }
+
+    let aidelay = 0.3;
+    aipaddle.style.top =
+        aipaddle.offsetTop + (ball.offsetTop - aipaddle.offsetTop - aipaddle.offsetHeight / 2) * aidelay + "px";
+    // ..............
+
+
 
     ball.style.left = ball.offsetLeft + Vx + "px";
     ball.style.top = ball.offsetTop + Vy + "px";
